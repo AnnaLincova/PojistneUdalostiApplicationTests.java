46a6c00fe2f2655e91309503c043523fcecf0848
Petr Jagr <ext.Petr.Jagr@skoda-auto.cz>
2022-03-16 13:35:37 +0100

    Revert "List of Smlouvy is accessible by calling "/smlouvy"."
    This reverts commit ea160abe237869c0cf409a807bbb3b236535b849.

--- src/main/java/alincova/springframework/pojistneudalosti/controllers/PojistenecController.java
+++ src/main/java/alincova/springframework/pojistneudalosti/controllers/PojistenecController.java
@@ -4,9 +4,7 @@ import alincova.springframework.pojistneudalosti.Pojisteni.Pojistenec;
 import alincova.springframework.pojistneudalosti.repositories.PojistenecRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PojistenecController {
